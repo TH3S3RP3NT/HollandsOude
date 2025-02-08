c5f043bbcf23eb233cc65fd6657ba821948b57e3
TH3S3RP3NT <119533606+TH3S3RP3NT@users.noreply.github.com>
2025-02-08 17:24:04 +0100

    update main.py

--- main.py
+++ main.py
@@ -5,13 +5,24 @@ from discord.ext import commands
 logger = settings.logging.getLogger('bot')
 
 def run():
-    intents = discord.Intents.default()
+    intents = discord.Intents.all()
     bot = commands.Bot(command_prefix='!', intents=intents)
 
     @bot.event
     async def on_ready():
         logger.info(f'User: {bot.user} (ID: {bot.user.id})')
-        print("_____________")
+
+
+    @bot.command(
+        aliases=['p'],
+        help="This command is for testing purposes",
+        description="This command is for testing purposes",
+        brief="testing purposes",
+        enable=False,
+        hidden=False
+    )
+    async def ping(ctx):
+        await ctx.send('Pong!')
 
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
